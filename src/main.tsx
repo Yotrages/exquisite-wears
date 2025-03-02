175a2afb563bf336ce06b218aeabebeb34f1a266
Yotrages <qayyumoladimeji@gmail.com>
2025-03-02 22:39:26 +0100

    ~~awesome: added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.tsx';
+import { Analytics } from "@vercel/analytics/react"
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>    
+    <Router>
+    <Analytics /> 
     <App /> 
     </Router>
   </StrictMode>,
-
 );
