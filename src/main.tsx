f93630ddb764779f75bc17f08d7aebb44cf9795d
Yotrages <qayyumoladimeji@gmail.com>
2025-04-01 15:58:13 +0100

    removed rubbish from main

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,12 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.tsx';
 import { Analytics } from "@vercel/analytics/react"
 import './index.css';
-import { Provider } from 'react-redux';
-import { store } from './redux/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
     <Router>
     <Analytics /> 
     <App /> 
     </Router>
-    </Provider>
   </StrictMode>,
 );
